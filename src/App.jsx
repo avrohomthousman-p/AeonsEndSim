e47769ce402657ae222b77aad46df92c9416f0c7
avrohomthousman-p <avrohomthousman@gmail.com>
2025-06-05 14:01:59 +0000

    feat: remove starter code

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import logo from '/icon.jpg'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
 			<div>
 				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
+					<img src={logo} className="logo" alt="logo" />
 				</a>
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
+			<h1>Aeons End Simulator</h1>
+			<div>
+				{/* TODO: make UI elements for the user to enter the character and cards they want */}
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	)
 }
