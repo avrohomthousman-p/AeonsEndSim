41de42c7611532ba6ab2f112a44816b9b0110cf6
avrohomthousman-p <avrohomthousman@gmail.com>
2025-06-06 19:15:09 +0000

    refactor: extract ui sections to seperate components

--- src/Simulator.jsx
+++ src/Simulator.jsx
@@ -4,6 +4,9 @@ import { FaArrowLeft } from 'react-icons/fa';
 import { useParams } from "react-router-dom"
 
 
+const BASE_URL = "https://storage.googleapis.com/aeons-end-pics/";
+
+
 function Simulator() {
     return (
         <div>
@@ -14,7 +17,7 @@ function Simulator() {
                 </a>
                 <h3 className='title'>Aeons End Simulator</h3>
 
-                <PlayerMat />
+                <PlayerArea />
             </div>
         </div>
     )
@@ -25,42 +28,67 @@ export default Simulator
 
 
 
-function PlayerMat() {
+function PlayerArea() {
     const { characterName } = useParams();
     const data = CHARACTERS[characterName];
-    const baseUrl = "https://storage.googleapis.com/aeons-end-pics/";
 
 
     return (
         <div>
-            <div id="breaches">
-                {
-                    data.breaches.map(
-                        (breachData) =>
-                            <img
-                                src={`${baseUrl}breaches/breach${breachData.breachID}-${breachData.orientation}.webp`}
-                                alt="breach"
-                                width="12%"
-                            />
-                    )
-                }
+            <BreachSection characterData={data} />
+            <CharacterSection characterName={characterName} />
+            <HandSection />
+        </div>
+    )
+}
+
+
+
+function BreachSection({ characterData }) {
+    return (
+        <div id="breaches">
+            {
+                characterData.breaches.map(
+                    (breachData) =>
+                        <img
+                            src={`${BASE_URL}breaches/breach${breachData.breachID}-${breachData.orientation}.webp`}
+                            alt="breach"
+                            width="12%"
+                        />
+                )
+            }
+        </div>
+    )
+}
+
+
+
+function CharacterSection({ characterName }) {
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%" }}>
+            <div style={{ display: "inline-block", width: "12%" }}>
+                <p>5 cards</p>
+                <img src={BASE_URL + "cards/cardBack.webp"} alt="deck" width="100%" />
             </div>
 
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%" }}>
-                <div style={{ display: "inline-block", width: "12%" }}>
-                    <p>5 cards</p>
-                    <img src={baseUrl + "cards/cardBack.webp"} alt="deck" width="100%" />
-                </div>
-
-                <div style={{ display: "inline-block", width: "35%" }} >
-                    <img src={baseUrl + "characters/" + characterName + ".webp"} alt="player mat" width="100%" />
-                </div>
-
-                <div style={{ display: "inline-block", width: "12%" }}>
-                    <p>0 cards</p>
-                    <img src={baseUrl + "cards/noCard.webp"} alt="discard pile" width="100%" />
-                </div>
+            <div style={{ display: "inline-block", width: "35%" }} >
+                <img src={BASE_URL + "characters/" + characterName + ".webp"} alt="player mat" width="100%" />
             </div>
+
+            <div style={{ display: "inline-block", width: "12%" }}>
+                <p>0 cards</p>
+                <img src={BASE_URL + "cards/noCard.webp"} alt="discard pile" width="100%" />
+            </div>
+        </div>
+    )
+}
+
+
+
+function HandSection(){
+    //TODO
+    return(
+        <div id="hand">
 
         </div>
     )
