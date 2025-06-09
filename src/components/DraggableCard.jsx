02c08df38f558394cd1432e55e5cd5488922e54b
avrohomthousman-p <avrohomthousman@gmail.com>
2025-06-09 03:07:25 +0000

    feat: setup drop zone to allow reordering cards in hand

--- src/components/DraggableCard.jsx
+++ src/components/DraggableCard.jsx
@@ -3,12 +3,16 @@ import { BASE_URL, DraggableCardType } from '../data/constants'
 
 
 /**
- * Represents a card in hand.
+ * Represents a card in hand, that can be moved out of your hand
+ * or to another place in your hand.
  */
-export default function DraggableCard({ cardName }) {
+export default function DraggableCard({ cardName, cardPosition }) {
     const [{ isDragging }, dragRef] = useDrag({
         type: DraggableCardType,
-        item: { cardName },
+        item: { 
+            "cardName": cardName,
+            "cardPosition": cardPosition,
+        },
         collect: (monitor) => ({
             isDragging: monitor.isDragging(),
         }),
