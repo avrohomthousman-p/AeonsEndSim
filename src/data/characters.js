d19f677b65b1a80b5d3048cd7d3f9848630ef052
avrohomthousman-p <avrohomthousman@gmail.com>
2025-06-15 19:54:37 +0000

    feat: added a lot of game data

--- src/data/characters.js
+++ src/data/characters.js
@@ -15,5 +15,55 @@ export const CHARACTERS = Object.freeze({
             { breachID: 4, orientation: 180 },
         ],
     }),
+    Adelheim: Object.freeze({
+        startingDeck: ["Crystal", "Crystal", "Crystal", "Crystal", "Crystal"],
+        startingHand: ["Amethyst_Shard", "Crystal", "Crystal", "Spark", "Spark"],
+        breaches: [
+            { breachID: 1, orientation: 360 },
+            { breachID: 2, orientation: 180 },
+            { breachID: 3, orientation: 0 },
+            { breachID: 4, orientation: 90 },
+        ],
+    }),
+    Brama: Object.freeze({
+        startingDeck: ["Spark", "Spark", "Crystal", "Crystal", "Crystal"],
+        startingHand: ["Buried_Light", "Crystal", "Crystal", "Crystal", "Crystal"],
+        breaches: [
+            { breachID: 1, orientation: 360 },
+            { breachID: 2, orientation: 90 },
+            { breachID: 3, orientation: 0 },
+            { breachID: 4, orientation: 180 },
+        ],
+    }),
+    Garu: Object.freeze({
+        startingDeck: ["Spark", "Spark", "Crystal", "Crystal", "Crystal"],
+        startingHand: ["Torch", "Crystal", "Crystal", "Crystal", "Spark"],
+        breaches: [
+            { breachID: 1, orientation: 360 },
+            { breachID: 2, orientation: 0 },
+            { breachID: 3, orientation: 90 },
+            { breachID: 4, orientation: 90 },
+        ],
+    }),
+    Jian: Object.freeze({
+        startingDeck: ["Spark", "Spark", "Crystal", "Crystal", "Crystal"],
+        startingHand: ["Moonstone_Shard", "Crystal", "Crystal", "Spark", "Spark"],
+        breaches: [
+            { breachID: 1, orientation: 360 },
+            { breachID: 2, orientation: 360 },
+            { breachID: 3, orientation: 0 },
+            { breachID: 4, orientation: 90 },
+        ],
+    }),
+    Lash: Object.freeze({
+        startingDeck: ["Spark", "Spark", "Crystal", "Crystal", "Crystal"],
+        startingHand: ["Quartz_Shard", "Crystal", "Crystal", "Crystal", "Spark"],
+        breaches: [
+            { breachID: 1, orientation: 360 },
+            { breachID: 2, orientation: 180 },
+            { breachID: 3, orientation: 0 },
+            { breachID: 4, orientation: 180 },
+        ],
+    }),
     // TODO: more characters
 });
