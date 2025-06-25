2656d48ca30190dad25778102d451072e5d24ccf
avrohomthousman-p <avrohomthousman@gmail.com>
2025-06-25 19:49:36 +0000

    feat: show highlighted cards in character picker

--- src/pages/CharacterSelector.jsx
+++ src/pages/CharacterSelector.jsx
@@ -1,18 +1,21 @@
-import logo from "/icon.jpg"
-import "./CharacterSelector.css"
-import { CHARACTERS } from "../data/characters"
-import { useNavigate } from "react-router-dom"
+import logo from "/icon.jpg";
+import { useState } from "react";
+import "./CharacterSelector.css";
+import { CHARACTERS } from "../data/characters";
+import { BASE_URL } from "../data/constants";
+import { useNavigate } from "react-router-dom";
 
 
 function CharacterSelectorPage() {
     return (
         <>
             <div>
-                <a href="https://vite.dev" target="_blank">
+                <h1>
                     <img src={logo} className="logo" alt="logo" />
-                </a>
+                    Aeons End Simulator
+                    <img src={logo} className="logo" alt="logo" />
+                </h1>
             </div>
-            <h1>Aeons End Simulator</h1>
             <div>
                 <p>Pick a character</p>
                 <CharacterPicker />
@@ -25,19 +28,30 @@ export default CharacterSelectorPage
 
 
 
-function CharacterPicker(){
+function CharacterPicker() {
     const navigate = useNavigate();
+    const [hoveredCharacter, setHoveredCharacter] = useState(null);
+
 
     return (
         <div>
-            {Object.keys(CHARACTERS).map((name) => (
-                <button 
-                    key={name}
-                    className="btn"
-                    onClick={ () => navigate(`/simulator/${name}`) }>
-                        
-                {name}</button>
-            ))}
+            <div>
+                {Object.keys(CHARACTERS).map((name) => (
+                    <button
+                        key={name}
+                        className="btn"
+                        onClick={() => navigate(`/simulator/${name}`)}
+                        onMouseEnter={() => setHoveredCharacter(name)}
+                        onMouseLeave={() => setHoveredCharacter(null)} >
+
+                        {name}</button>
+                ))}
+            </div>
+            <div style={{ marginTop: "10px" }}>
+                <img 
+                    src={hoveredCharacter ? `${BASE_URL}characters/${hoveredCharacter}.webp` : null}
+                    style={{ height: "50vh" }} />
+            </div>
         </div>
     )
 }
